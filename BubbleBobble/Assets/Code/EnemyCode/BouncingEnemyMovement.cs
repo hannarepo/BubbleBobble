4e2d2bcf8fdce33069caf62a0364bd76f9175374
Santeri Haronoja <111272852+SantruMan@users.noreply.github.com>
2024-11-05 13:11:14 +0200

    Bouncable enemy updates
    The enemy now bounces with physics.

--- BubbleBobble/Assets/Code/EnemyCode/BouncingEnemyMovement.cs
+++ BubbleBobble/Assets/Code/EnemyCode/BouncingEnemyMovement.cs
@@ -4,44 +4,46 @@ using UnityEngine;
 
 namespace BubbleBobble
 {
-    public class BouncingEnemyMovement : MonoBehaviour
-    {
+	public class BouncingEnemyMovement : MonoBehaviour
+	{
 
-        [SerializeField] private float _speed = 5;
-        private Transform _transform;
-        private Vector2 _velocity = Vector2.zero;
+		[SerializeField] private float _speed = 5;
+		private Rigidbody2D _rigidbody;
+		private Transform _transform;
+		private Vector2 _velocity = Vector2.zero;
+		
+		void Start()
+		{
+			_rigidbody = GetComponent<Rigidbody2D>();
+			_rigidbody.AddForce((Vector2.up + Vector2.right).normalized * _speed, ForceMode2D.Impulse);
+			//_transform = transform;
+			//_velocity = (Vector2.left + Vector2.up * 1.5f).normalized;
+		}
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            _transform = transform;
-            _velocity = (Vector2.left + Vector2.up * 1.5f).normalized;
-        }
+		/*
+		void Update()
+		{
+			_transform.position += new Vector3(_velocity.x, _velocity.y, 0) * _speed * Time.deltaTime;
+		}
 
-        // Update is called once per frame
-        void Update()
-        {
-            _transform.position += new Vector3(_velocity.x, _velocity.y, 0) * _speed * Time.deltaTime;
-        }
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			BounceObject wall = other.GetComponent<BounceObject>();
+			if (wall == null)
+			{
+				return;
+			}
 
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            BounceObject _object = other.GetComponent<BounceObject>();
-            if (_object == null)
-            {
-                return;
-            }
+			Vector2 normal = wall.Normal;
+			Bounce(normal);
+		}
 
-            Vector2 normal = _object.Normal;
-            Bounce(normal);
-        }
-
-        public void Bounce(Vector2 normal)
-        {
-            Vector2 u = Vector2.Dot(_velocity, normal) * normal;
-            Vector2 w = _velocity - u;
-            _velocity = w - u;
-        }
-    }
-
+		public void Bounce(Vector2 normal)
+		{
+			Vector2 u = Vector2.Dot(_velocity, normal) * normal;
+			Vector2 w = _velocity - u;
+			_velocity = w - u;
+		}
+		*/
+	}
 }
--- BubbleBobble/Assets/Code/EnemyCode/EnemyInvincibility.cs
+++ BubbleBobble/Assets/Code/EnemyCode/EnemyInvincibility.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 namespace BubbleBobble
 {
-    public class EnemyInvincibility : MonoBehaviour
-    {
-        private bool _isInvincible = false;
+	public class EnemyInvincibility : MonoBehaviour
+	{
+		private bool _isInvincible = false;
 
-        public bool IsInvincible { get { return _isInvincible; } }
+		public bool IsInvincible { get { return _isInvincible; } }
 
-        public void FreeEnemy()
-        {
-            _isInvincible = true;
-        }
+		public void FreeEnemy()
+		{
+			_isInvincible = true;
+		}
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
