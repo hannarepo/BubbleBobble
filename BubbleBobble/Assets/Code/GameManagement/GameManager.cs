511bbef6c08571c5892569311c14fd26adc1c7ff
Jose <jose.mantyla@tuni.fi>
2024-11-06 12:01:17 +0200

    Changed EnemyTestScript renamed to EnemyManagement
    Fixed an error in player control script, fixed bubble movement

--- BubbleBobble/Assets/Code/GameManagement/GameManager.cs
+++ BubbleBobble/Assets/Code/GameManagement/GameManager.cs
@@ -125,7 +125,7 @@ namespace BubbleBobble
 			// Destroy all enemies on screen at index 0
 			for (int i = _enemyList.Count - 1; i >= 0; i--)
 			{
-				_enemyList[0].GetComponent<EnemyTestScript>().Die();
+				_enemyList[0].GetComponent<EnemyManagement>().Die();
 			}
 
 			TrappedEnemyBubble[] trappedEnemies = FindObjectsOfType<TrappedEnemyBubble>();
