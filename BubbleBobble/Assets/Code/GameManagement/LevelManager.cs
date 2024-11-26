dcac6985e88827dfc07b98db6c89854d84eed84f
Jose <jose.mantyla@tuni.fi>
2024-11-26 13:36:32 +0200

    Level1 Intro stuff
    Level1 starts with the intro and enemies appear after the intro is done.
    TODO: Restrict player movement until the intro is done.

--- BubbleBobble/Assets/Code/GameManagement/LevelManager.cs
+++ BubbleBobble/Assets/Code/GameManagement/LevelManager.cs
@@ -48,7 +48,7 @@ namespace BubbleBobble
 
 		private void Update()
 		{
-			if (_levelChanger.IsLevelLoaded)
+			if (_levelChanger.IsLevelStarted)
 			{
 				_spawnTimer += Time.deltaTime;
 				_hurryUpTimer += Time.deltaTime;
