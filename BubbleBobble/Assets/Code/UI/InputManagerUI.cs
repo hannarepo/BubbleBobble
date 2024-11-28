ed7d678d09c31f897f0b9ba3afe69c62f7ff666a
RaivoKoira <linecardriver@gmail.com>
2024-11-28 06:06:01 +0200

    Complete Ingame UI navigation with buttons
    + GameoverScreen refining

--- BubbleBobble/Assets/Code/UI/InputManagerUI.cs
+++ BubbleBobble/Assets/Code/UI/InputManagerUI.cs
@@ -5,12 +5,23 @@ using UnityEngine.InputSystem;
 
 namespace BubbleBobble
 {
+
+    /// <summary>
+    /// Listen for inputs that are used to open and close the menu and store.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// author: Juho Kokkonen
+    /// </remarks>
     public class InputManagerUI : MonoBehaviour
     {
         public static InputManagerUI instance;
         public bool MenuOpenCloseInput { get; private set; }
         private PlayerInput _playerInput;
         private InputAction _menuOpenCloseAction;
+
+        public bool StoreOpenCloseInput { get; private set; }
+        private InputAction _storeOpenCloseAction;
 
 
         private void Awake()
@@ -22,11 +33,13 @@ namespace BubbleBobble
 
             _playerInput = GetComponent<PlayerInput>();
             _menuOpenCloseAction = _playerInput.actions["Pause"];
+            _storeOpenCloseAction = _playerInput.actions["OpenShop"];
         }
 
         private void Update()
         {
             MenuOpenCloseInput = _menuOpenCloseAction.WasPressedThisFrame();
+            StoreOpenCloseInput = _storeOpenCloseAction.WasPressedThisFrame();
         }
     }
 }
