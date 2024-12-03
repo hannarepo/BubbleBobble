8d2fb2e2678c53449da8e9646d0f3d73723d6c21
Hanna Repo <hannarepo91@gmail.com>
2024-12-03 13:24:59 +0200

    Undefeatable enemy

--- BubbleBobble/Assets/Code/UI/CurrentLevelText.cs
+++ BubbleBobble/Assets/Code/UI/CurrentLevelText.cs
@@ -7,18 +7,25 @@ namespace BubbleBobble
 	{
 		[SerializeField] private TMP_Text _levelNumberText;
 		[SerializeField] private TMP_Text _worldNumberText;
-		private int _levelCounter = 1;
+		[SerializeField] private int _transition1Number = 12;
+		[SerializeField] private int _transition2Number = 23;
+		private int _levelNumber = 1;
+		private int _worldNumber = 1;
 
 		public void UpdateLevelNumber()
 		{
-			_levelCounter++;
-			_levelNumberText.text = $"{_levelCounter}";
+			_levelNumber++;
+			if (_levelNumber == _transition1Number || _levelNumber == _transition2Number)
+			{
+				_levelNumber = 0;
+			}
+			_levelNumberText.text = $"{_levelNumber}";
 		}
 
-		public void UpdateWorldNumber(int worldIndex)
+		public void UpdateWorldNumber()
 		{
-			_levelCounter = -1;
-			_worldNumberText.text = $"{worldIndex}";
+			_worldNumber++;
+			_worldNumberText.text = $"{_worldNumber}";
 		}
 	}
 }
