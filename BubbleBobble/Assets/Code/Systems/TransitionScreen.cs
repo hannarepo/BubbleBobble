b183e7fe09b7d6bbc768ed6fc6d9633bad09b918
Jose <jose.mantyla@tuni.fi>
2024-12-04 14:29:24 +0200

    Added new comments to LevelChanger and created image animation

--- BubbleBobble/Assets/Code/Systems/TransitionScreen.cs
+++ BubbleBobble/Assets/Code/Systems/TransitionScreen.cs
@@ -1,31 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace BubbleBobble
 {
 	public class TransitionScreen : MonoBehaviour
 	{
-		[SerializeField] private float _levelChangeDelay = 1f;
-		private LevelChanger _levelChanger;
-		private bool _levelChangeCalled = false;
-		private void Start()
+		[SerializeField] private float _speed = 1f;
+		[SerializeField] private float _endYPosition = 1f;
+		[SerializeField] private MainMenu _mainMenu;
+		[SerializeField] private PauseMenu _pauseMenu;
+		private AudioSource _audioSource;
+		private bool _startMovement = false;
+
+		private void Awake()
 		{
-			_levelChanger = FindObjectOfType<LevelChanger>();
+			_audioSource = GetComponent<AudioSource>();
 		}
+
 
 		private void Update()
 		{
-			if (_levelChanger.IsLevelLoaded && !_levelChangeCalled)
+			if (_startMovement)
 			{
-				Invoke("DelayedLevelChange",_levelChangeDelay);
-				_levelChangeCalled = true;
+				transform.position += Vector3.up * _speed * Time.deltaTime;
+				
+				if (transform.position.y > _endYPosition)
+				{
+					ChangeScene();
+					_startMovement = false;
+				}
 			}
 		}
 
-		private void DelayedLevelChange()
+		private void ChangeScene()
 		{
-			_levelChanger.LoadLevel();
+			print("Changing scene");
+			if (_mainMenu != null)
+			{
+				_mainMenu.PlayGame();
+			}
+			else if (_pauseMenu != null)
+			{
+				_pauseMenu.Home();
+			}
+		}
+
+		public void StartSceneTransition()
+		{
+			_audioSource.Play();
+			_startMovement = true;
 		}
 	}
 }
